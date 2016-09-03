20757836c883921e37681adaf0e431487e158749
Sujan Shrestha <shrsujan2007@gmail.com>
2016-09-03 20:28:50 +0545

    5. config added with property versions, imported to users to console api endpoint pattern

--- controllers/api/v1/users/index.js
+++ controllers/api/v1/users/index.js
@@ -1,7 +1,12 @@
 'use strict';
 
+var path = require('path');
+var basePath = path.resolve();
+var config = require(basePath + '/config/config');
+var v1 = config.versions.v1.prefix;
+
 module.exports = function(app){
 
-    // APIs for user module
+    console.log(v1 + '/user');
 
 };
